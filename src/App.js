47e4615ff4f9d4ddac4f042eb531c2054d7cd1ba
cseguier <126275268+cseguier@users.noreply.github.com>
2023-03-17 14:13:45 -0400

    Perfect DL button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import camo from './camo.png'
+import cv from './cv.pdf'
 import './App.css';
+import './button.css'
+
+
 
 function App() {
   return (
@@ -8,36 +12,64 @@ function App() {
         <img src={camo} className="App-logo" alt="logo" />
       </header>
 
-      <div className='text'>
+      <div className='bodyContainer'>
         <p>
           Welcome to the Camo experience.
         </p>
 
-        <p>
+        <a className="arc"
+          href={cv}
+          target="_blank"
+          rel="noopener noreferrer"
+          download>
+          Download my Resumey
+        </a>
+
+
+
+
+        <div className='linksContainer'>
+
           <a className="App-link"
             href="https://github.com/cseguier"
             target="_blank"
             rel="noopener noreferrer" >
             Github Pro
-          </a> ___ <a className="App-link"
+          </a>
+
+          <br />
+
+          <a className="App-link"
             href=" https://github.com/mushve"
             target="_blank"
             rel="noopener noreferrer" >
             Github Study
-          </a> ___ <a className="App-link"
+          </a>
+
+          <br />
+
+          <a className="App-link"
             href="https://cseguier-connect4.netlify.app/"
             target="_blank"
             rel="noopener noreferrer" >
             Connect4
-          </a> ___ <a className="App-link"
+          </a>
+
+          <br />
+
+          <a className="App-link"
             href={camo}
             target="_blank"
             rel="noopener noreferrer" >
             CatCafé
           </a>
-        </p>
-      </div>
 
+        </div>
+
+
+
+
+      </div>
     </div>
   );
 }
